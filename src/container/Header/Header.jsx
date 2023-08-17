3919e856c2efdbf75997dec6f23ea94ba2cde2d6
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-08-17 21:32:58 +0300

    add MUI

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,6 +1,12 @@
 import "./Header.scss";
 
 const Header = () => {
-  return <div className="header">Header</div>;
+  return (
+    <div className="header">
+      <div className="header_menu">menu</div>
+      <div className="header_name">name</div>
+      <div className="header_cart">cart</div>
+    </div>
+  );
 };
 export default Header;
