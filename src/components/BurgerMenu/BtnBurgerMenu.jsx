c3f67802c1302f576f4085df0c0c992a435bf053
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-08-23 10:54:25 +0300

    animate btn burger

--- src/components/BurgerMenu/BtnBurgerMenu.jsx
+++ src/components/BurgerMenu/BtnBurgerMenu.jsx
@@ -7,7 +7,9 @@ const BurgerMenu = () => {
   const isOpen = useSelector((data) => data.burgerMenu.isOpen);
 
   return (
-    <div className="burger-menu" onClick={() => dispatch(toggleMenu())}>
+    <div
+      className={`burger-menu ${isOpen ? "active" : ""}`}
+      onClick={() => dispatch(toggleMenu())}>
       <div className="burger-menu-line one"></div>
       <div className="burger-menu-line two"></div>
       <div className="burger-menu-line three"></div>
