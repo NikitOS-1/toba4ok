471cca935f978ac7a4f0e43d8581e52e762e3f5c
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-08-28 19:51:16 +0300

    1

--- src/container/App.js
+++ src/container/App.js
@@ -10,7 +10,6 @@ function App() {
       <video autoPlay loop muted playsInline autoFocus className="video-bg">
         <source src={video} type="video/mp4" />
       </video>
-      1
       <Header className="lg" />
       <Main />
       <Footer />
