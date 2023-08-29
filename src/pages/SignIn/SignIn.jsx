348d85b1d96776bd8f60f4ced39e1096ccd85f53
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-08-29 20:01:07 +0300

    update style fo signIn modal

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,19 +1,43 @@
 import "./SignIn.scss";
 import Modal from "../../components/Modal/Modal";
+import { useState } from "react";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
 const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div className="sign-in_container">
-      <Modal>
-        <div>
+    <Modal>
+      <div className="sign-in_container">
+        <div className="sign-in_content">
           <h1>Sign In</h1>
-          <input type="text" />
-          <input type="text" />
-          <button>Login</button>
-          <a href="#">Forgot password</a>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail((prev) => (prev = e.target.value))}
+          />
+          <div>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword((prev) => (prev = e.target.value))}
+            />
+            <div className="see_pass">
+              <RemoveRedEyeIcon />
+            </div>
+          </div>
+
+          <div className="btn-login">
+            <button>Login</button>
+          </div>
+          <div className="forgot_pass">
+            <a href="#">Forgot password</a>
+          </div>
         </div>
-      </Modal>
-    </div>
+      </div>
+    </Modal>
   );
 };
 export default SignIn;
