abd81cc44cf5e04a062f703c6ccc219d76d07638
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-08-30 16:12:51 +0300

    add google btn

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,50 +1,65 @@
 import "./SignIn.scss";
 import Modal from "../../components/Modal/Modal";
-import { useState } from "react";
+
+import {
+  useState
+}
+
+from "react";
 import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import GoogleButton from "react-google-button";
 
-const SignIn = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [typePass, setTypePass] = useState(false);
+const SignIn=()=> {
+  const [email,
+  setEmail]=useState("");
+  const [password,
+  setPassword]=useState("");
+  const [typePass,
+  setTypePass]=useState(false);
 
-  const seePass = () => {
-    setTypePass((prev) => !prev);
-  };
+  const seePass=()=> {
+    setTypePass((prev)=> !prev);
+  }
 
-  return (
-    <Modal>
-      <div className="sign-in_container">
-        <div className="sign-in_content">
-          <h1>Sign In</h1>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail((prev) => (prev = e.target.value))}
-            placeholder="Email"
-          />
-          <div>
-            <input
-              type={typePass ? "text" : "password"}
-              value={password}
-              onChange={(e) => setPassword((prev) => (prev = e.target.value))}
-              placeholder="Password"
-            />
-            <div className="see_pass" onClick={seePass}>
-              {typePass ? <VisibilityOffIcon /> : <RemoveRedEyeIcon />}
-            </div>
-          </div>
+  ;
 
-          <div className="btn-login">
-            <button>Login</button>
-          </div>
-          <div className="forgot_pass">
-            <a href="#">Forgot password?</a>
-          </div>
-        </div>
-      </div>
-    </Modal>
-  );
-};
+  return (<Modal> <div className="sign-in_container"> <div className="sign-in_content"> <h1>Sign In</h1> <input type="email"
+
+    value= {
+      email
+    }
+
+    onChange= {
+      (e)=> setEmail((prev)=> (prev=e.target.value))
+    }
+
+    placeholder="Email"
+
+    /> <div> <input type= {
+      typePass ? "text" : "password"
+    }
+
+    value= {
+      password
+    }
+
+    onChange= {
+      (e)=> setPassword((prev)=> (prev=e.target.value))
+    }
+
+    placeholder="Password"
+
+    /> <div className="see_pass"onClick= {
+      seePass
+    }
+
+    > {
+      typePass ? <VisibilityOffIcon /> : <RemoveRedEyeIcon />
+    }
+
+    </div> </div> <div className="btn-login"> <button>Login</button> </div> <div className="forgot_pass"> <a href="#">Forgot password?</a> </div> <div className="line_with_text"></div> <GoogleButton /> </div> </div> </Modal>);
+}
+
+;
 export default SignIn;
