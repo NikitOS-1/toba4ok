19535df01d2cc1270db99265a390c2212053e04b
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-08-30 19:06:22 +0300

    ref style

--- src/components/ParallaxRow/ParallaxRow.jsx
+++ src/components/ParallaxRow/ParallaxRow.jsx
@@ -1,14 +1,18 @@
 import "./ParallaxRow.css";
 
-const ParallaxRow = ({ imageUrl, title, description }) => {
+const ParallaxRow = ({ imageUrl, title, description, revers = false }) => {
   const rowStyle = {
     backgroundImage: `url(${imageUrl})`,
   };
   return (
-    <div className="parallax-row" style={rowStyle}>
-      <div className="content">
-        <h2>{title}</h2>
-        <p>{description}</p>
+    <div className="parallax-wrap">
+      <div className="parallax-row" style={rowStyle}>
+        <div
+          className="content"
+          style={revers ? { marginLeft: "40%" } : { marginRight: "40%" }}>
+          <h2>{title}</h2>
+          <p>{description}</p>
+        </div>
       </div>
     </div>
   );
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,65 +1,26 @@
 import { useEffect, useState } from "react";
+import ParallaxRow from "../../components/ParallaxRow/ParallaxRow";
 import "./HomePage.scss";
 
 const HomePage = () => {
   const img1 = process.env.PUBLIC_URL + "/images/kalyan1.jpg";
-
-  const [scrollPosition, setScrollPosition] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const position = window.screenY;
-      setScrollPosition(position);
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  const backgroundSize = 100 + scrollPosition / 5;
+  const img2 = process.env.PUBLIC_URL + "/images/kalyan2.jpg";
+  let text1 =
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam neque ducimus quia labore debitis quod. Doloremque repellat perferendis, fugit porro, aliquid placeat laudantium debitis voluptatum incidunt assumenda pariatur saepe provident orem ipsum dolor sit amet, consectetur adipisicing elit. Ullam neque ducimus quia labore debitis quod. Doloremque repellat perferendis, fugit porro, aliquid placeat laudantium debitis voluptatum incidunt assumenda pariatur saepe provident";
 
   return (
     <div className="container">
-      <div className="wrap_paralax">
-        <div className="row1">
-          <div
-            className="picture_paralax"
-            style={{
-              backgroundImage: `url(${img1})`,
-              // backgroundSize: `${backgroundSize}%`,
-            }}></div>
-          <div className="info">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
-            accusamus id voluptatibus? Distinctio velit libero recusandae veniam
-            eligendi saepe excepturi placeat nemo obcaecati? Modi, laudantium
-            delectus! Cupiditate quod quae iste. Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Ipsam accusamus id voluptatibus?
-            Distinctio velit libero recusandae veniam eligendi saepe excepturi
-            placeat nemo obcaecati? Modi, laudantium delectus! Cupiditate quod
-            quae iste. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Ipsam accusamus id voluptatibus? Distinctio velit libero recusandae
-            veniam eligendi saepe excepturi placeat nemo obcaecati? Modi,
-            laudantium delectus! Cupiditate quod quae iste.
-          </div>
-        </div>
-        <div className="row2">
-          <div className="info">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
-            accusamus id voluptatibus? Distinctio velit libero recusandae veniam
-            eligendi saepe excepturi placeat nemo obcaecati? Modi, laudantium
-            delectus! Cupiditate quod quae iste. Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Ipsam accusamus id voluptatibus?
-            Distinctio velit libero recusandae veniam eligendi saepe excepturi
-            placeat nemo obcaecati? Modi, laudantium delectus! Cupiditate quod
-            quae iste. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Ipsam accusamus id voluptatibus? Distinctio velit libero recusandae
-            veniam eligendi saepe excepturi placeat nemo obcaecati? Modi,
-            laudantium delectus! Cupiditate quod quae iste.
-          </div>
-          <div className="picture_paralax"></div>
-        </div>
-      </div>
+      <ParallaxRow
+        imageUrl={img1}
+        title="Заголовок"
+        description={text1}
+        revers={true}
+      />
+      <ParallaxRow
+        imageUrl={img2}
+        title="Заголовок 1"
+        description={text1 + text1 + text1}
+      />
     </div>
   );
 };
