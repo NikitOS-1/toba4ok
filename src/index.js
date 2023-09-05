ed8d5f1669df1d2a23b7e5dbe5add1546a53d59f
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-09-05 16:17:54 +0300

    .

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from "../src/container/App";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import "./firebase";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
