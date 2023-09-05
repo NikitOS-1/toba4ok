60fa2fb48d631611fa9053c33dba3406cd766a4d
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-09-05 17:57:22 +0300

    fix to react hook form and ref area name

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -4,8 +4,16 @@ import Modal from "../../components/Modal/Modal";
 import { useState } from "react";
 import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import { useForm } from "react-hook-form";
 
 const SignUp = () => {
+  const {
+    register,
+    formState: { errors, isValid },
+    handleSubmit,
+    reset,
+  } = useForm({ mode: "onBlur" });
+
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -37,45 +45,72 @@ const SignUp = () => {
   return (
     <Modal>
       <div className="sign-up_container">
-        <div className="sign-up_content">
-          <h1>Sign Up</h1>
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName((prev) => (prev = e.target.value))}
-            placeholder="Name"
-          />
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail((prev) => (prev = e.target.value))}
-            placeholder="Email"
-          />
-          <input
-            type={typePass ? "text" : "password"}
-            value={password1}
-            onChange={(e) => setPassword1((prev) => (prev = e.target.value))}
-            placeholder="Password"
-          />
-          <div>
+        <form>
+          <div className="sign-up_content">
+            <h1>Sign Up</h1>
+            <label className="name">
+              <input
+                type="text"
+                {...register("name", {
+                  required: "You need to fill in a name",
+                  minLength: {
+                    value: 3,
+                    message: "You entered too short a name",
+                  },
+                })}
+                value={name}
+                onChange={(e) => setName((prev) => (prev = e.target.value))}
+                placeholder="Name"
+              />
+            </label>
+
+            <div className="error">
+              {errors?.name && (
+                <p style={{ color: "tomato" }}>
+                  {errors?.name?.message || "Error!"}
+                </p>
+              )}
+            </div>
+
+            <label className="email">
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail((prev) => (prev = e.target.value))}
+                placeholder="Email"
+              />
+            </label>
+
             <input
               type={typePass ? "text" : "password"}
-              value={password2}
-              onChange={(e) => setPassword2((prev) => (prev = e.target.value))}
-              placeholder="Repeat password"
+              value={password1}
+              onChange={(e) => setPassword1((prev) => (prev = e.target.value))}
+              placeholder="Password"
             />
-            <div className="see_pass" onClick={seePass}>
-              {typePass ? <VisibilityOffIcon /> : <RemoveRedEyeIcon />}
+            <div>
+              <input
+                type={typePass ? "text" : "password"}
+                value={password2}
+                onChange={(e) =>
+                  setPassword2((prev) => (prev = e.target.value))
+                }
+                placeholder="Repeat password"
+              />
+              <div className="see_pass" onClick={seePass}>
+                {typePass ? <VisibilityOffIcon /> : <RemoveRedEyeIcon />}
+              </div>
+            </div>
+            <div className="agree">
+              <input type="checkbox" name="" id="" />
+              <span>I agree to the processing of my information</span>
+            </div>
+            <div className="btn-create">
+              <button type="submit" onClick={createAccount} disabled={!isValid}>
+                Create an account
+              </button>
             </div>
           </div>
-          <div className="agree">
-            <input type="checkbox" name="" id="" />
-            <span>I agree to the processing of my information</span>
-          </div>
-          <div className="btn-create">
-            <button onClick={createAccount}>Create an account</button>
-          </div>
-        </div>
+        </form>
       </div>
     </Modal>
   );
