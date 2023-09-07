0ae52c022c9e6574d3c47bee5ab0b4ae0cf11bf6
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-09-07 20:04:08 +0300

    compleated log-in-up

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,6 +1,19 @@
+import { getAuth } from "firebase/auth";
 import "./UserPage.scss";
 
 const UserPage = () => {
-  return <div>UserPage</div>;
+  const auth = getAuth();
+  console.log(auth);
+
+  return (
+    <div className="user_container">
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint
+        dignissimos unde suscipit nulla quisquam corporis modi similique quos
+        eveniet consequuntur? Nemo ipsam officia quisquam porro saepe qui iusto
+        reiciendis suscipit?
+      </p>
+    </div>
+  );
 };
 export default UserPage;
