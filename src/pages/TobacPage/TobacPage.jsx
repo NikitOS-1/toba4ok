824b2bcce17e7c794f618166de2e30bb1869b3e5
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-09-11 21:29:27 +0300

    add data with brand tobaco

--- src/pages/TobacPage/TobacPage.jsx
+++ src/pages/TobacPage/TobacPage.jsx
@@ -1,6 +1,28 @@
+import { brandTobaco } from "../../redux/data_BrandTobaco";
 import "./TobacPage.scss";
 
 const TobacPage = () => {
-  return <div>TobacPage</div>;
+  console.log(brandTobaco.length);
+  return (
+    <div>
+      TobacPage
+      <div
+        style={{
+          margin: "0 auto",
+          display: "flex",
+          flexWrap: "wrap",
+          width: "80%",
+          alignItems: "center",
+          justifyContent: "center",
+        }}>
+        {brandTobaco.map((el, i) => (
+          <div key={i}>
+            <img src={el.logoURl} alt="Logo" />
+            <div>{el.brand}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default TobacPage;
---
+++ src/redux/data_BrandTobaco.js
@@ -0,0 +1,112 @@
+export const brandTobaco = [
+  {
+    brand: "4:20",
+    logoURl: "./images/tobac_brand/kupit-tabak-dlya-kalyana-4-20-160x120.png",
+  },
+  {
+    brand: "5ive",
+    logoURl: "./images/tobac_brand/tabak-5ive-kupit-160x120.jpg",
+  },
+  {
+    brand: "Absolem",
+    logoURl: "./images/tobac_brand/absolem_kupit_tabak---160x120.png",
+  },
+  {
+    brand: "Adalya",
+    logoURl: "./images/tobac_brand/tabak-adalya1-160x120.jpg",
+  },
+  {
+    brand: "Afzal",
+    logoURl: "./images/tobac_brand/afzaL-160x120.jpg",
+  },
+  {
+    brand: "Asti",
+    logoURl: "./images/tobac_brand/Kupit-Tabak-dlja-kaljana-Asti-160x120.png",
+  },
+  {
+    brand: "Buta",
+    logoURl: "./images/tobac_brand/kalyannyj-tabak-buta-kupit-min-160x120.png",
+  },
+  {
+    brand: "Chefir",
+    logoURl: "./images/tobac_brand/tabak_chefir_kupit-160x120.png",
+  },
+  {
+    brand: "CULTt",
+    logoURl: "./images/tobac_brand/tabak-CULT-kupit-new-160x120.jpg",
+  },
+  {
+    brand: "Daily Hookah",
+    logoURl: "./images/tobac_brand/DailyHookah-160x120.jpg",
+  },
+  {
+    brand: "Daim",
+    logoURl: "./images/tobac_brand/daim-160x120.png",
+  },
+  {
+    brand: "Dead Horse",
+    logoURl: "./images/tobac_brand/dead-horse-kupit-tabak-new-160x120.png",
+  },
+  {
+    brand: "Duft",
+    logoURl: "./images/tobac_brand/Duft_tabak-160x120.jpg",
+  },
+  {
+    brand: "Fumari",
+    logoURl: "./images/tobac_brand/tabak-fumari-kupit-160x120.png",
+  },
+  {
+    brand: "Fusion",
+    logoURl: "./images/tobac_brand/fusion-tabak-160x120.jpg",
+  },
+  {
+    brand: "Indigo Smoke",
+    logoURl: "./images/tobac_brand/tabak_INDIGO_SMOKE-160x120.jpg",
+  },
+  {
+    brand: "Jibiar",
+    logoURl: "./images/tobac_brand/Tabak-Jibiar-(Dzhibiar)-160x120.jpg",
+  },
+  {
+    brand: "Lirra",
+    logoURl: "./images/tobac_brand/tabak-lirra-kupit-160x120.png",
+  },
+  {
+    brand: "Loud",
+    logoURl: "./images/tobac_brand/loud-kupit-tabak-160x120.png",
+  },
+  {
+    brand: "Milano",
+    logoURl: "./images/tobac_brand/Tabak-Milano-160x120.jpg",
+  },
+  {
+    brand: "Molfar",
+    logoURl: "./images/tobac_brand/Molfar-160x120.png",
+  },
+  {
+    brand: "NUAL",
+    logoURl: "./images/tobac_brand/tabak-nual-160x120.png",
+  },
+  {
+    brand: "Serbetli",
+    logoURl: "./images/tobac_brand/serbetli-tabak-kupit-160x120.png",
+  },
+  {
+    brand: "Spectrum",
+    logoURl:
+      "./images/tobac_brand/tabak-spectrum-obzor-vkusy-tsena-160x120.jpg",
+  },
+  {
+    brand: "Swipe",
+    logoURl: "./images/tobac_brand/tabak_swipe_kupit-160x120.png",
+  },
+  {
+    brand: "Tangiers",
+    logoURl:
+      "./images/tobac_brand/Kupit'-tabak-dlja-kal'jana-Tangiers-160x120.jpg",
+  },
+  {
+    brand: "Unity Urban Collection",
+    logoURl: "./images/tobac_brand/tabak-unity1-160x120.png",
+  },
+];
