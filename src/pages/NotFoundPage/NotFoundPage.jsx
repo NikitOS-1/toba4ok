992d6ff0e6ea31269be9877503f7843c7a9fe37a
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-09-25 21:41:37 +0300

    compleated Not Found Page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,4 +1,16 @@
+import "./NotFoundPage.scss";
+
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <div className="wrap_notFound">
+      <div className="title">
+        <h1>We are sorry,</h1>
+        <h3>but the page you were looking for can't be found.</h3>
+      </div>
+      <div className="desc">
+        <h1>404</h1>
+      </div>
+    </div>
+  );
 };
 export default NotFoundPage;
