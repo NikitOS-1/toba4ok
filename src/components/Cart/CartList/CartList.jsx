20ef2b9d29c975e295ce5d904dfc2b22e29a69ee
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-09-25 21:56:09 +0300

    ref

--- src/components/Cart/CartList/CartList.jsx
+++ src/components/Cart/CartList/CartList.jsx
@@ -1,6 +1,10 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./CartList.scss";
 
 const CartList = () => {
-  return <div>CartList</div>;
+  const dispatch = useDispatch();
+  const isOpen = useSelector((data) => data.cartToggle.isOpen);
+
+  return <div className="wrap_cart">CartList</div>;
 };
 export default CartList;
