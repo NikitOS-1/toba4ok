ec4651171ca3551c55b46421eb0dfef9aafd1f26
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-09-26 16:28:54 +0300

    add arrow back

--- src/pages/TobacPage/TobacPage.jsx
+++ src/pages/TobacPage/TobacPage.jsx
@@ -1,13 +1,18 @@
 import { useSelector } from "react-redux";
 import "./TobacPage.scss";
 import CardBrandTobac from "../../components/CardBrandTobac/CardBrandTobac";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { ArrowBack } from "@mui/icons-material";
 
 const TobacPage = () => {
   const data = useSelector((state) => state.dataProduct.data);
-
+  const navigate = useNavigate();
   return (
     <div>
+      <nav className="btn-back">
+        <ArrowBack />
+        <div onClick={() => navigate(-1)}>Back</div>
+      </nav>
       <div className="tobac_container">
         {data.map((el, i) => (
           <CardBrandTobac brand={el.brand} logoURl={el.logoURl} key={i} />
